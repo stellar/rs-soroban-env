187621b1b192efcc488c3d050e51bf95cc450f24
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-05-06 13:12:28 -0700

    Make panic handler a feature

--- stellar-contract-env-common/src/rt.rs
+++ stellar-contract-env-common/src/rt.rs
@@ -11,17 +11,7 @@ pub fn trap() -> ! {
     panic!()
 }
 
-// Handle panic provides a panic handler that traps using the wasm unreachable
-// opcode, which is less expensive than the builtin panic handling.
-//
-// Not used when std feature is enabled and when targeting wasm because std
-// provides a panic handler. Note that std may be loaded even if the std feature
-// is off, such as when tests are built. The guard on the wasm target prevents
-// the handler from being loaded in those other cases. Note that adding a
-// not(test) here would not be sufficient since not(test) is only true for the
-// crate that the test resides in, and not for crates running tests dependent on
-// this crate.
-#[cfg(all(target_family = "wasm", not(feature = "std")))]
+#[cfg(feature = "panic_handler")]
 #[panic_handler]
 fn handle_panic(_: &core::panic::PanicInfo) -> ! {
     trap();
