1563dfced4a382b5230ec4220fc1c37e984e3a86
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-05-06 18:19:27 -0700

    Revert two commits

--- stellar-contract-env-common/src/env_obj.rs
+++ stellar-contract-env-common/src/env_obj.rs
@@ -3,17 +3,26 @@ use super::{xdr::ScObjectType, Env, EnvVal, HasEnv, RawObj, RawVal, RawValType, 
 // EnvObj is just an EnvVal that is statically guaranteed (by construction) to refer
 // to Tag::Object, so it's safe to call methods on it that are meaningful to objects.
 #[derive(Clone)]
-pub struct EnvObj<E: Env> {
-    pub env: E,
-    pub obj: RawObj,
-}
+pub struct EnvObj<E: Env>(EnvVal<E>);
 
 impl<E: Env> HasEnv<E> for EnvObj<E> {
     fn env(&self) -> &E {
-        &self.env
+        &self.0.env
     }
     fn mut_env(&mut self) -> &mut E {
-        &mut self.env
+        &mut self.0.env
+    }
+}
+
+impl<E: Env> AsRef<EnvVal<E>> for EnvObj<E> {
+    fn as_ref(&self) -> &EnvVal<E> {
+        &self.0
+    }
+}
+
+impl<E: Env> AsMut<EnvVal<E>> for EnvObj<E> {
+    fn as_mut(&mut self) -> &mut EnvVal<E> {
+        &mut self.0
     }
 }
 
@@ -21,37 +30,35 @@ impl<E: Env> TryFrom<EnvVal<E>> for EnvObj<E> {
     type Error = ();
 
     fn try_from(ev: EnvVal<E>) -> Result<Self, Self::Error> {
-        let obj: RawObj = ev.val.try_into()?;
-        Ok(EnvObj { env: ev.env, obj })
+        match ev.val.get_tag() {
+            Tag::Object => Ok(EnvObj(ev)),
+            _ => Err(()),
+        }
     }
 }
 
 impl<E: Env> Into<EnvVal<E>> for EnvObj<E> {
     fn into(self) -> EnvVal<E> {
-        EnvVal {
-            env: self.env,
-            val: self.obj.into(),
-        }
+        self.0
     }
 }
 
 impl<E: Env> Into<RawVal> for EnvObj<E> {
     fn into(self) -> RawVal {
-        self.obj.into()
+        self.0.into()
     }
 }
 
 impl<E: Env> Into<RawObj> for EnvObj<E> {
     fn into(self) -> RawObj {
-        self.obj
+        unsafe { <RawObj as RawValType>::unchecked_from_val(self.0.val) }
     }
 }
 
 impl<E: Env> EnvObj<E> {
     #[inline(always)]
     pub fn get_handle(&self) -> u32 {
-        let val: &RawVal = self.obj.as_ref();
-        val.get_major()
+        self.0.val.get_major()
     }
 
     // NB: we don't provide a "get_type" to avoid casting a bad bit-pattern
@@ -61,13 +68,11 @@ impl<E: Env> EnvObj<E> {
     #[inline(always)]
     pub fn from_type_and_handle(ty: ScObjectType, handle: u32, env: E) -> EnvObj<E> {
         let val = unsafe { RawVal::from_major_minor_and_tag(handle, ty as u32, Tag::Object) };
-        let obj = unsafe { RawObj::unchecked_from_val(val) };
-        EnvObj { env, obj }
+        EnvObj(EnvVal { env, val })
     }
 
     #[inline(always)]
     pub fn is_obj_type(&self, ty: ScObjectType) -> bool {
-        let val: &RawVal = self.obj.as_ref();
-        val.has_minor(ty as u32)
+        self.0.val.has_minor(ty as u32)
     }
 }
