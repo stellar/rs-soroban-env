87b8c5053a8e3c2aa9113721fdb30ba72a087847
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-08-18 22:39:34 -0700

    u8 only

--- soroban-env-common/src/array.rs
+++ soroban-env-common/src/array.rs
@@ -1,61 +1,52 @@
 use stellar_xdr::ScObjectType;
 
-use crate::{
-    ConversionError, Env, EnvVal, IntoVal, Object, RawVal, RawValConvertible, TryFromVal,
-    TryIntoVal,
-};
+use crate::{ConversionError, Env, EnvVal, IntoVal, Object, RawVal, RawValConvertible, TryIntoVal};
 
 use seq_macro::seq;
 
-macro_rules! impl_for_array {
+macro_rules! impl_for_u8_array {
     ( $count:literal ) => {
-        impl<E: Env, T> TryFrom<EnvVal<E, RawVal>> for [T; $count]
-        where
-            T: TryFrom<EnvVal<E, RawVal>>,
-        {
+        impl<E: Env> TryFrom<EnvVal<E, RawVal>> for [u8; $count] {
             type Error = ConversionError;
 
             fn try_from(ev: EnvVal<E, RawVal>) -> Result<Self, Self::Error> {
                 const COUNT: u32 = $count;
-                if !Object::val_is_obj_type(ev.val, ScObjectType::Vec) {
+                if !Object::val_is_obj_type(ev.val, ScObjectType::Bytes) {
                     return Err(ConversionError);
                 }
                 let env = ev.env.clone();
-                let vec = unsafe { Object::unchecked_from_val(ev.val) };
-                let len: u32 = env.vec_len(vec).try_into()?;
+                let bin = unsafe { Object::unchecked_from_val(ev.val) };
+                let len: u32 = env.binary_len(bin).try_into()?;
                 if len != COUNT {
                     return Err(ConversionError);
                 }
+                // TODO: Use memory copy instead of individual gets.
                 Ok(seq!(INDEX in 0..$count {
                     [
                         #({
                             const IDX: u32 = INDEX;
-                            T::try_from_val(&env, env.vec_get(vec, IDX.into())).map_err(|_| ConversionError)?
+                            let val = env.binary_get(bin, IDX.into());
+                            let val_u32 = unsafe { u32::unchecked_from_val(val) };
+                            val_u32 as u8
                         },)*
                     ]
                 }))
             }
         }
 
-        impl<E: Env, T> IntoVal<E, RawVal> for [T; $count]
-        where
-            T: IntoVal<E, RawVal>
-        {
+        impl<E: Env> IntoVal<E, RawVal> for [u8; $count] {
             fn into_val(self, env: &E) -> RawVal {
-                const COUNT: u32 = $count;
                 let env = env.clone();
-                let mut vec = env.vec_new(COUNT.into());
-                for t in self {
-                    vec = env.vec_push(vec, t.into_val(&env));
+                let mut bin = env.binary_new();
+                for b in self {
+                    let b_u32 = Into::<u32>::into(b);
+                    bin = env.binary_push(bin, b_u32.into_val(&env));
                 }
-                vec.to_raw()
+                bin.to_raw()
             }
         }
 
-        impl<E: Env, T> IntoVal<E, EnvVal<E, RawVal>> for [T; $count]
-        where
-            T: IntoVal<E, RawVal>
-        {
+        impl<E: Env> IntoVal<E, EnvVal<E, RawVal>> for [u8; $count] {
             fn into_val(self, env: &E) -> EnvVal<E, RawVal> {
                 let rv: RawVal = self.into_val(env);
                 EnvVal{
@@ -65,26 +56,21 @@ macro_rules! impl_for_array {
             }
         }
 
-        impl<E: Env, T> TryIntoVal<E, [T; $count]> for RawVal
-        where
-            T: TryFrom<EnvVal<E, RawVal>>
-        {
+        impl<E: Env> TryIntoVal<E, [u8; $count]> for RawVal {
             type Error = ConversionError;
             #[inline(always)]
-            fn try_into_val(self, env: &E) -> Result<[T; $count], Self::Error> {
+            fn try_into_val(self, env: &E) -> Result<[u8; $count], Self::Error> {
                 EnvVal{ env: env.clone(), val: self }.try_into()
             }
         }
-
-        // TODO: Add [u8; $count] impls as well to and from Bytes.
     };
 }
 
 seq!(COUNT in 0..31 {
-    impl_for_array! { COUNT }
+    impl_for_u8_array! { COUNT }
 });
-impl_for_array! { 32 }
-impl_for_array! { 64 }
-impl_for_array! { 128 }
-impl_for_array! { 256 }
-impl_for_array! { 512 }
+impl_for_u8_array! { 32 }
+impl_for_u8_array! { 64 }
+impl_for_u8_array! { 128 }
+impl_for_u8_array! { 256 }
+impl_for_u8_array! { 512 }
