0c1f241a571bd0901c58a8b7f110f1ee37bea9e6
Jay Geng <jay@stellar.org>
2022-08-19 11:21:26 -0400

    Add a `contract_event` test

--- soroban-env-host/src/test.rs
+++ soroban-env-host/src/test.rs
@@ -9,6 +9,7 @@ mod vec;
 
 #[cfg(feature = "vm")]
 mod budget_metering;
+mod contract_event;
 #[cfg(feature = "vm")]
 mod invocation;
 #[cfg(all(feature = "vm", feature = "testutils"))]
---
+++ soroban-env-host/src/test/contract_event.rs
@@ -0,0 +1,65 @@
+use crate::{
+    events::HostEvent,
+    xdr::{
+        ContractEvent, ContractEventBody, ContractEventType, ContractEventV0, ExtensionPoint, Hash,
+        ScMap, ScMapEntry, ScObject::Map, ScVal,
+    },
+    ContractFunctionSet, Env, EnvBase, Host, HostError, RawVal, Symbol,
+};
+use std::rc::Rc;
+
+pub struct EmptyContractWithEvents;
+
+impl ContractFunctionSet for EmptyContractWithEvents {
+    fn call(&self, _func: &Symbol, host: &Host, _args: &[RawVal]) -> Option<RawVal> {
+        // Add a contract event
+        let mut data = host.map_new();
+        data = host.map_put(data, 1_u32.into(), 2_u32.into());
+        let mut topics = host.vec_new(().into());
+        topics = host.vec_push(topics, 0u32.into());
+        topics = host.vec_push(topics, 1u32.into());
+        Some(host.contract_event(topics, data.to_raw()))
+    }
+}
+
+#[test]
+fn contract_event() -> Result<(), HostError> {
+    let host = Host::default();
+    let dummy_id = [0; 32];
+    let mut id = host.binary_new();
+    id = host.binary_copy_from_slice(id, 0u32.into(), &dummy_id);
+    let test_contract = Rc::new(EmptyContractWithEvents {});
+    let sym = Symbol::from_str("add");
+    let args = host.test_vec_obj::<i32>(&[1, 2])?;
+    host.register_test_contract(id, test_contract)?;
+    host.call(id, sym.into(), args.into());
+
+    let event_ref = ContractEvent {
+        ext: ExtensionPoint::V0,
+        contract_id: Hash(dummy_id),
+        type_: ContractEventType::Contract,
+        body: ContractEventBody::V0(ContractEventV0 {
+            topics: host.map_err(vec![ScVal::U32(0), ScVal::U32(1)].try_into())?,
+            data: ScVal::Object(Some(Map(host.map_err(ScMap::try_from(vec![
+                ScMapEntry {
+                    key: ScVal::U32(1),
+                    val: ScVal::U32(2),
+                },
+            ]))?))),
+        }),
+    };
+
+    // Fish out the last contract event and check that it is
+    // correct, and formats as expected.
+    let events = host.get_events();
+    match events.0.last() {
+        Some(HostEvent::Contract(ce)) => {
+            //println!("{:?}", ce);
+            assert_eq!(*ce, event_ref)
+        }
+        _ => {
+            panic!("missing contract event")
+        }
+    };
+    Ok(())
+}
