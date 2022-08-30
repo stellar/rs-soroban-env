9711029ae066c73850e87ee65ed35acd88c0b00d
Jay Geng <jay@stellar.org>
2022-08-30 11:13:55 -0400

    Style fix

--- soroban-test-wasms/wasm-workspace/create_contract/src/lib.rs
+++ soroban-test-wasms/wasm-workspace/create_contract/src/lib.rs
@@ -8,7 +8,6 @@ impl Contract {
     // Note that anyone can create a contract here with any salt, so a users call to
     // this could be frontrun and the same salt taken.
     pub fn create(e: Env, wasm: Bytes, salt: Bytes) {
-        let deployer = e.deployer().from_current_contract(&salt);
-        let _contract_id = deployer.deploy(&wasm);
+        e.deployer().from_current_contract(&salt).deploy(&wasm);
     }
 }
