9aa0ce2ada1cb9eebb9d46273eb560693731d813
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-09-16 15:47:38 -0700

    Change the encoding of Result to only support Status

--- soroban-env-common/src/result.rs
+++ soroban-env-common/src/result.rs
@@ -1,52 +1,42 @@
-use crate::{ConversionError, Env, IntoVal, RawVal, Symbol, TryFromVal, TryIntoVal};
+use crate::{ConversionError, Env, IntoVal, RawVal, Status, TryFromVal, TryIntoVal};
 
-const SYMBOL_OK: Symbol = Symbol::from_str("Ok");
-const SYMBOL_OK_PAYLOAD: u64 = SYMBOL_OK.to_raw().get_payload();
-
-const SYMBOL_ERROR: Symbol = Symbol::from_str("Err");
-const SYMBOL_ERROR_PAYLOAD: u64 = SYMBOL_ERROR.to_raw().get_payload();
-
-impl<E: Env, T, F> TryFromVal<E, RawVal> for Result<T, F>
+impl<E: Env, T> TryFromVal<E, RawVal> for Result<T, Status>
 where
     T: TryFromVal<E, RawVal, Error = ConversionError>,
-    F: TryFromVal<E, RawVal, Error = ConversionError>,
 {
     type Error = ConversionError;
 
     #[inline(always)]
     fn try_from_val(env: &E, val: RawVal) -> Result<Self, Self::Error> {
-        let (discriminant, value): (RawVal, RawVal) = val.try_into_val(env)?;
-        match discriminant.get_payload() {
-            SYMBOL_OK_PAYLOAD => Ok(Ok(T::try_from_val(env, value)?)),
-            SYMBOL_ERROR_PAYLOAD => Ok(Err(F::try_from_val(env, value)?)),
-            _ => Err(ConversionError),
+        if let Ok(status) = Status::try_from_val(env, val) {
+            Ok(Err(status))
+        } else {
+            Ok(Ok(T::try_from_val(env, val)?))
         }
     }
 }
 
-impl<E: Env, T, F> TryIntoVal<E, Result<T, F>> for RawVal
+impl<E: Env, T> TryIntoVal<E, Result<T, Status>> for RawVal
 where
     T: TryFromVal<E, RawVal, Error = ConversionError>,
-    F: TryFromVal<E, RawVal, Error = ConversionError>,
 {
     type Error = ConversionError;
 
     #[inline(always)]
-    fn try_into_val(self, env: &E) -> Result<Result<T, F>, Self::Error> {
+    fn try_into_val(self, env: &E) -> Result<Result<T, Status>, Self::Error> {
         <_ as TryFromVal<E, RawVal>>::try_from_val(env, self)
     }
 }
 
-impl<E: Env, T, F> IntoVal<E, RawVal> for Result<T, F>
+impl<E: Env, T> IntoVal<E, RawVal> for Result<T, Status>
 where
     T: IntoVal<E, RawVal>,
-    F: IntoVal<E, RawVal>,
 {
     #[inline(always)]
     fn into_val(self, env: &E) -> RawVal {
         match self {
-            Ok(t) => (SYMBOL_OK, t).into_val(env),
-            Err(f) => (SYMBOL_ERROR, f).into_val(env),
+            Ok(t) => t.into_val(env),
+            Err(status) => status.into_val(env),
         }
     }
 }
@@ -59,8 +49,8 @@ where
     #[inline(always)]
     fn into_val(self, env: &E) -> RawVal {
         match self {
-            Ok(t) => (SYMBOL_OK, t).into_val(env),
-            Err(f) => (SYMBOL_ERROR, f).into_val(env),
+            Ok(t) => t.into_val(env),
+            Err(status) => status.into_val(env),
         }
     }
 }
