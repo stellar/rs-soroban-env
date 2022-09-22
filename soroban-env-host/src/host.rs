4734b59a8a4178c5ffb55b334e347c0ee07f3f99
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-09-21 21:03:25 -0700

    fix fmt

--- soroban-env-host/src/host.rs
+++ soroban-env-host/src/host.rs
@@ -1093,7 +1093,9 @@ impl VmCallerCheckedEnv for Host {
             )?;
             Ok(RawVal::from_void())
         } else {
-            Err(self.err_general("log_fmt_values unsupported outside of debug-assertion builds of the host"))
+            Err(self.err_general(
+                "log_fmt_values unsupported outside of debug-assertion builds of the host",
+            ))
         }
     }
 
