03bd5a1f30cf36c89bd1d5357d6693734864d665
Graydon Hoare <graydon@pobox.com>
2022-09-22 12:42:44 -0700

    Add initial "hostile contract" and unit test calling it

--- soroban-env-host/src/test.rs
+++ soroban-env-host/src/test.rs
@@ -15,6 +15,8 @@ mod budget_metering;
 #[cfg(feature = "testutils")]
 mod contract_event;
 #[cfg(feature = "vm")]
+mod hostile;
+#[cfg(feature = "vm")]
 mod invocation;
 #[cfg(all(feature = "vm", feature = "testutils"))]
 mod lifecycle;
