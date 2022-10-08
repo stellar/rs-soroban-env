776a70e6cf43281e57d784b20b078a3514c4b9d0
Dmytro Kozhevin <dmytro@stellar.org>
2022-10-07 20:10:10 -0400

    Format fix

--- soroban-env-host/src/native_contract/token/allowance.rs
+++ soroban-env-host/src/native_contract/token/allowance.rs
@@ -2,7 +2,7 @@ use crate::host::Host;
 use crate::native_contract::base_types::BigInt;
 use crate::native_contract::token::public_types::Identifier;
 use crate::native_contract::token::storage_types::{AllowanceDataKey, DataKey};
-use crate::{HostError, err};
+use crate::{err, HostError};
 use core::cmp::Ordering;
 use soroban_env_common::{CheckedEnv, TryIntoVal};
 
