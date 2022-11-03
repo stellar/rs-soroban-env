744472c15f4a06587b0e8ff2c11e923ee26e9ded
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-11-02 20:29:39 -0700

    Disable default-features on stellar-xdr workspace dependency (#559)

---
+++ soroban-env-common/tests/no_std/src/lib.rs
@@ -0,0 +1,19 @@
+#![no_std]
+
+/// This test crate simulates a package that imports soroban_env_common and adds
+/// a panic handler. The importing package will be allowed to do that if no
+/// panic handler has been already linked in. If soroban_env_common imports
+/// libstd by accident then this crate will fail to build because there will be
+/// two panic hanlders.
+
+// Import a type from soroban_env_common so that the compiler includes it in the
+// build.
+#[allow(unused_imports)]
+use soroban_env_common::Env as _;
+
+// Import a panic handler to collide with any accidentally included libstd panic
+// handler.
+#[panic_handler]
+fn handle_panic(_: &core::panic::PanicInfo) -> ! {
+    unreachable!()
+}
