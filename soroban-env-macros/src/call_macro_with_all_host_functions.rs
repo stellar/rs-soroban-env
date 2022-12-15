8109fd48b1c53c84236895255bb220089226289d
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-12-15 00:33:37 -0800

    add comments

--- soroban-env-macros/src/call_macro_with_all_host_functions.rs
+++ soroban-env-macros/src/call_macro_with_all_host_functions.rs
@@ -10,42 +10,54 @@ use crate::path;
 pub fn generate(file_lit: LitStr) -> Result<TokenStream, Error> {
     let file_str = file_lit.value();
     let file_path = path::abs_from_rel_to_manifest(&file_str);
+
     let file = File::open(&file_path).map_err(|e| {
         Error::new(
             file_lit.span(),
             format!("error reading file '{}': {}", file_str, e),
         )
     })?;
+
     let modules: Vec<Module> = serde_json::from_reader(file).map_err(|e| {
         Error::new(
             file_lit.span(),
             format!("error parsing file '{}': {}", file_str, e),
         )
     })?;
+
+    // Build the 'mod' sections.
     let modules = modules.iter().map(|m| {
         let name = format_ident!("{}", &m.name);
         let export = &m.export;
+
+        // Build the 'fn' sections within the 'mod'.
         let functions = m.functions.iter().map(|f| {
             let docs = f.docs.as_deref().unwrap_or_default();
             let export = &f.export;
             let name = format_ident!("{}", &f.name);
+
+            // Build the args for use within the 'fn'.
             let args = f.args.iter().map(|a| {
                 let name = format_ident!("{}", &a.name);
                 let r#type = format_ident!("{}", &a.r#type);
                 quote! { #name: #r#type }
             });
+
             let r#return = format_ident!("{}", &f.r#return);
+
             quote! {
                 #[doc = #docs]
                 { #export, fn #name(#(#args),*) -> #r#return }
             }
         });
+
         quote! {
             mod #name #export {
                 #(#functions)*
             }
         }
     });
+
     Ok(quote! {
         #[doc(hidden)]
         #[macro_export]
