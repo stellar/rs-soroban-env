3eff14811dabe109727488c96d9d993a6c1e3d9b
Dmytro Kozhevin <dmytro@stellar.org>
2023-01-30 19:22:23 -0500

    Added a simple account contract and updated the tests contracts/Wasm files.

---
+++ soroban-test-wasms/wasm-workspace/simple_account/src/lib.rs
@@ -0,0 +1,42 @@
+#![no_std]
+
+struct SimpleAccount;
+
+use soroban_account::AuthorizationContext;
+use soroban_sdk::{contractimpl, contracttype, BytesN, Env, IntoVal, Vec};
+
+#[derive(Clone)]
+#[contracttype]
+pub enum DataKey {
+    Owner,
+}
+
+#[contractimpl]
+impl SimpleAccount {
+    pub fn init(env: Env, public_key: BytesN<32>) {
+        env.storage().set(&DataKey::Owner, &public_key);
+    }
+
+    pub fn set_owner(env: Env, new_owner: BytesN<32>) {
+        env.current_contract_address()
+            .require_auth((new_owner.clone(),).into_val(&env));
+        env.storage().set(&DataKey::Owner, &new_owner);
+    }
+
+    pub fn check_auth(
+        env: Env,
+        signature_payload: BytesN<32>,
+        signature_args: Vec<BytesN<64>>,
+        _auth_context: Vec<AuthorizationContext>,
+    ) {
+        if signature_args.len() != 1 {
+            panic!("incorrect number of signature args");
+        }
+        let public_key: BytesN<32> = env.storage().get(&DataKey::Owner).unwrap().unwrap();
+        env.crypto().ed25519_verify(
+            &public_key,
+            &signature_payload.into(),
+            &signature_args.get(0).unwrap().unwrap(),
+        );
+    }
+}
