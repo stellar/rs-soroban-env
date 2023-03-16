9abd6295b1906a38169dcd238de1ae3864d81302
Jay Geng <jay@stellar.org>
2023-03-15 23:50:32 -0400

    fixup! Use `mem::forget` to remove sample deallocation from runner

--- soroban-env-host/src/cost_runner/cost_types/val_xdr_conv.rs
+++ soroban-env-host/src/cost_runner/cost_types/val_xdr_conv.rs
@@ -7,6 +7,8 @@ impl CostRunner for ValXdrConvRun {
     type SampleType = (RawVal, ScVal);
 
     fn run_iter(host: &crate::Host, _iter: u64, sample: Self::SampleType) {
+        // Here we don't need to `forget` the sample since its size is fixed (a single shallow Val).
+        // The deallocation has been accounted for by the baseline measurement.
         host.from_host_val(sample.0).unwrap();
         host.to_host_val(&sample.1).unwrap();
     }
