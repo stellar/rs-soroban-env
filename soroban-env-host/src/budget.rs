0f405c6111754043f33058ad60c54fab2adb3ab2
Jay Geng <jay@stellar.org>
2023-03-20 19:57:25 -0400

    Update the InvokeHostParameter

--- soroban-env-host/src/budget.rs
+++ soroban-env-host/src/budget.rs
@@ -454,10 +454,7 @@ impl Default for BudgetImpl {
                 // this and some are much less.
                 CostType::WasmInsnExec => cpu.lin_param = 32, // TODO: needs to be tiered
                 CostType::WasmMemAlloc => cpu.const_param = 6000, // TODO: questionable
-                CostType::InvokeHostFunction => {
-                    cpu.const_param = 5455;
-                    cpu.lin_param = 702
-                }
+                CostType::InvokeHostFunction => cpu.lin_param = 714,
                 CostType::VisitObject => cpu.lin_param = 208,
                 CostType::ValXdrConv => cpu.lin_param = 255,
                 CostType::ValSer => cpu.lin_param = 77,
