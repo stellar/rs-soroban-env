4b95746eb67b9b014606c9a5ac5bd66f5a534b6e
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2023-04-28 15:22:10 -0700

    expand comment

--- soroban-env-host/src/auth.rs
+++ soroban-env-host/src/auth.rs
@@ -522,7 +522,11 @@ impl AuthorizationManager {
             .iter()
             .filter(|t| t.authenticated)
             .filter_map(|t| {
-                // Ignore authorizations without an address as they are implied.
+                // Ignore authorizations without an address as they are implied,
+                // less useful as a test utility, and not succinctly capturable
+                // in the list of tuples. This is a tradeoff between offering up
+                // all authorizations vs the authorizations developers will
+                // mostly care about.
                 t.address.as_ref().map(|a| {
                     (
                         a.clone(),
