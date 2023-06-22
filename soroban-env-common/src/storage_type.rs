97604e600985584e87c76a9a328e2a73e930d227
Siddharth Suresh <siddharth@stellar.org>
2023-06-22 10:40:28 -0700

    Allowing cloning storage type

--- soroban-env-common/src/storage_type.rs
+++ soroban-env-common/src/storage_type.rs
@@ -6,7 +6,7 @@ use crate::declare_wasmi_marshal_for_enum;
 /// This is just a distinct enum local to the env interface that is used as
 /// an argument to storage functions. It doesn't correspond to any [`Val`] types,
 /// and is passed by direct marshalling as a u64.
-#[derive(Debug, FromPrimitive, PartialEq, Eq)]
+#[derive(Debug, FromPrimitive, PartialEq, Eq, Clone)]
 pub enum StorageType {
     Temporary = 0,
     Persistent = 1,
