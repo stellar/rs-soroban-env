3269e90f03c71f6560428c6d22ae4f0501621d07
Siddharth Suresh <siddharth@stellar.org>
2023-07-06 11:05:45 -0700

    Use host.err for auth error

--- soroban-env-host/src/auth.rs
+++ soroban-env-host/src/auth.rs
@@ -591,10 +591,12 @@ impl AuthorizationManager {
         }
         // No matching tracker found, hence the invocation isn't
         // authorized.
-        Err(HostError::from((
+        Err(host.err(
             ScErrorType::Auth,
             ScErrorCode::InvalidAction,
-        )))
+            "Not authorized",
+            &[],
+        ))
     }
 
     fn require_auth_internal(
