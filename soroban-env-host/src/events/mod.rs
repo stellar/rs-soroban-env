99fe0840283512358ae25a08739a19d534653561
Jay Geng <jay@stellar.org>
2023-07-19 18:54:24 -0400

    Remove event topic limits

--- soroban-env-host/src/events/mod.rs
+++ soroban-env-host/src/events/mod.rs
@@ -134,12 +134,6 @@ impl core::fmt::Display for HostEvent {
 #[derive(Clone, Debug, Default)]
 pub struct Events(pub Vec<HostEvent>);
 
-// Maximum number of topics in a `ContractEvent`. This applies to both
-// `Contract` and `System` types of contract events.
-pub(crate) const CONTRACT_EVENT_TOPICS_LIMIT: usize = 4;
-// Maximum number of bytes in a topic `Bytes`.
-pub(crate) const TOPIC_BYTES_LENGTH_LIMIT: usize = 64;
-
 impl Host {
     pub(crate) fn with_events_mut<F, U>(&self, f: F) -> Result<U, HostError>
     where
@@ -159,7 +153,6 @@ impl Host {
         topics: VecObject,
         data: Val,
     ) -> Result<(), HostError> {
-        self.validate_contract_event_topics(topics)?;
         let ce = InternalContractEvent {
             type_,
             contract_id: self.bytesobj_from_internal_contract_id()?,
