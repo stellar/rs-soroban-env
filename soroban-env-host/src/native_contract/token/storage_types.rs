c875329011ef991c60badb7559c1b193a8c3afab
Siddharth Suresh <siddharth@stellar.org>
2023-10-11 17:14:07 -0700

    Update BALANCE_BUMP_AMOUNT

--- soroban-env-host/src/native_contract/token/storage_types.rs
+++ soroban-env-host/src/native_contract/token/storage_types.rs
@@ -6,8 +6,8 @@ pub(crate) const DAY_IN_LEDGERS: u32 = 17280;
 pub(crate) const INSTANCE_EXTEND_AMOUNT: u32 = 7 * DAY_IN_LEDGERS;
 pub(crate) const INSTANCE_TTL_THRESHOLD: u32 = INSTANCE_EXTEND_AMOUNT - DAY_IN_LEDGERS;
 
-pub(crate) const BALANCE_BUMP_AMOUNT: u32 = 30 * DAY_IN_LEDGERS;
-pub(crate) const BALANCE_TTL_THRESHOLD: u32 = BALANCE_BUMP_AMOUNT - DAY_IN_LEDGERS;
+pub(crate) const BALANCE_EXTEND_AMOUNT: u32 = 30 * DAY_IN_LEDGERS;
+pub(crate) const BALANCE_TTL_THRESHOLD: u32 = BALANCE_EXTEND_AMOUNT - DAY_IN_LEDGERS;
 
 #[contracttype]
 pub struct AllowanceDataKey {
