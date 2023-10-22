61f1b3039490577cd57cf94ad1300ecb485bd798
Jay Geng <jay@stellar.org>
2023-10-22 18:03:09 -0400

    Refactoring: split `budget.rs` into several files

---
+++ soroban-env-host/src/budget/model.rs
@@ -0,0 +1,124 @@
+use crate::{
+    xdr::{ContractCostParamEntry, ScErrorCode, ScErrorType},
+    HostError,
+};
+use core::fmt::{Debug, Display};
+
+/// We provide a "cost model" object that evaluates a linear expression:
+///
+///    f(x) = a + b * Option<x>
+///
+/// Where a, b are "fixed" parameters at construction time (extracted from an
+/// on-chain cost schedule, so technically not _totally_ fixed) and Option<x>
+/// is some abstract input variable -- say, event counts or object sizes --
+/// provided at runtime. If the input cannot be defined, i.e., the cost is
+/// constant, input-independent, then pass in `None` as the input.
+///
+/// The same `CostModel` type, i.e. `CostType` (applied to different parameters
+/// and variables) is used for calculating memory as well as CPU time.
+///
+/// The various `CostType`s are carefully choosen such that 1. their underlying
+/// cost characteristics (both cpu and memory) at runtime can be described
+/// sufficiently by a linear model and 2. they together encompass the vast
+/// majority of available operations done by the `env` -- the host and the VM.
+///
+/// The parameters for a `CostModel` are calibrated empirically. See this crate's
+/// benchmarks for more details.
+pub trait HostCostModel {
+    fn evaluate(&self, input: Option<u64>) -> Result<u64, HostError>;
+
+    #[cfg(test)]
+    fn reset(&mut self);
+}
+
+/// The number of bits to scale the linear term by. The linear coefficient has
+/// been scaled by this factor during parameter fitting to retain more significant
+/// digits. Thus to get the cost from the raw input, we need to scale the result
+/// back by the same factor.
+pub const COST_MODEL_LIN_TERM_SCALE_BITS: u32 = 7;
+
+/// A helper type that wraps an u64 to signify the wrapped value have been scaled.
+#[derive(Clone)]
+pub(crate) struct ScaledU64(pub(crate) u64);
+
+impl ScaledU64 {
+    pub const fn unscale(self) -> u64 {
+        self.0 >> COST_MODEL_LIN_TERM_SCALE_BITS
+    }
+
+    #[cfg(test)]
+    pub const fn from_unscaled_u64(u: u64) -> Self {
+        ScaledU64(u << COST_MODEL_LIN_TERM_SCALE_BITS)
+    }
+
+    pub const fn is_zero(&self) -> bool {
+        self.0 == 0
+    }
+
+    pub const fn saturating_mul(&self, rhs: u64) -> Self {
+        ScaledU64(self.0.saturating_mul(rhs))
+    }
+}
+
+impl Display for ScaledU64 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl Debug for ScaledU64 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Scaled({})", self.0)
+    }
+}
+
+#[derive(Clone, Debug)]
+pub(crate) struct MeteredCostComponent {
+    pub(crate) const_term: u64,
+    pub(crate) lin_term: ScaledU64,
+}
+
+impl TryFrom<&ContractCostParamEntry> for MeteredCostComponent {
+    type Error = HostError;
+
+    fn try_from(entry: &ContractCostParamEntry) -> Result<Self, Self::Error> {
+        if entry.const_term < 0 || entry.linear_term < 0 {
+            return Err((ScErrorType::Context, ScErrorCode::InvalidInput).into());
+        }
+        Ok(MeteredCostComponent {
+            const_term: entry.const_term as u64,
+            lin_term: ScaledU64(entry.linear_term as u64),
+        })
+    }
+}
+
+impl TryFrom<ContractCostParamEntry> for MeteredCostComponent {
+    type Error = HostError;
+
+    fn try_from(entry: ContractCostParamEntry) -> Result<Self, Self::Error> {
+        Self::try_from(&entry)
+    }
+}
+
+impl HostCostModel for MeteredCostComponent {
+    fn evaluate(&self, input: Option<u64>) -> Result<u64, HostError> {
+        let const_term = self.const_term;
+        match input {
+            Some(input) => {
+                let mut res = const_term;
+                if !self.lin_term.is_zero() {
+                    let lin_cost = self.lin_term.saturating_mul(input).unscale();
+                    res = res.saturating_add(lin_cost)
+                }
+                Ok(res)
+            }
+            None => Ok(const_term),
+        }
+    }
+
+    #[cfg(test)]
+    fn reset(&mut self) {
+        self.const_term = 0;
+        self.lin_term = ScaledU64(0);
+    }
+}
---
+++ soroban-env-host/src/budget/util.rs
@@ -0,0 +1,121 @@
+use super::Budget;
+use crate::{host::error::TryBorrowOrErr, HostError};
+
+#[cfg(test)]
+use super::model::ScaledU64;
+#[cfg(test)]
+use crate::xdr::ContractCostType;
+
+#[cfg(test)]
+impl Budget {
+    pub fn reset_models(&self) -> Result<(), HostError> {
+        self.mut_budget(|mut b| {
+            b.cpu_insns.reset_models();
+            b.mem_bytes.reset_models();
+            Ok(())
+        })
+    }
+
+    pub(crate) fn override_model_with_scaled_params(
+        &self,
+        ty: ContractCostType,
+        const_cpu: u64,
+        lin_cpu: ScaledU64,
+        const_mem: u64,
+        lin_mem: ScaledU64,
+    ) -> Result<(), HostError> {
+        let mut bgt = self.0.try_borrow_mut_or_err()?;
+
+        let cpu_model = bgt.cpu_insns.get_cost_model_mut(ty);
+        cpu_model.const_term = const_cpu;
+        cpu_model.lin_term = lin_cpu;
+
+        let mem_model = bgt.mem_bytes.get_cost_model_mut(ty);
+        mem_model.const_term = const_mem;
+        mem_model.lin_term = lin_mem;
+        Ok(())
+    }
+
+    pub(crate) fn override_model_with_unscaled_params(
+        &self,
+        ty: ContractCostType,
+        const_cpu: u64,
+        lin_cpu: u64,
+        const_mem: u64,
+        lin_mem: u64,
+    ) -> Result<(), HostError> {
+        self.override_model_with_scaled_params(
+            ty,
+            const_cpu,
+            ScaledU64::from_unscaled_u64(lin_cpu),
+            const_mem,
+            ScaledU64::from_unscaled_u64(lin_mem),
+        )
+    }
+
+    pub(crate) fn track_wasm_mem_alloc(&self, delta: u64) -> Result<(), HostError> {
+        let mut bgt = self.0.try_borrow_mut_or_err()?;
+        bgt.tracker.wasm_memory = bgt.tracker.wasm_memory.saturating_add(delta);
+        Ok(())
+    }
+
+    pub fn get_wasm_mem_alloc(&self) -> Result<u64, HostError> {
+        Ok(self.0.try_borrow_or_err()?.tracker.wasm_memory)
+    }
+}
+
+#[cfg(any(test, feature = "testutils"))]
+impl Budget {
+    pub fn reset_default(&self) -> Result<(), HostError> {
+        *self.0.try_borrow_mut_or_err()? = super::BudgetImpl::default();
+        Ok(())
+    }
+
+    pub fn reset_unlimited(&self) -> Result<(), HostError> {
+        self.reset_unlimited_cpu()?;
+        self.reset_unlimited_mem()?;
+        Ok(())
+    }
+
+    pub fn reset_unlimited_cpu(&self) -> Result<(), HostError> {
+        self.mut_budget(|mut b| {
+            b.cpu_insns.reset(u64::MAX);
+            Ok(())
+        })?; // panic means multiple-mut-borrow bug
+        self.reset_tracker()
+    }
+
+    pub fn reset_unlimited_mem(&self) -> Result<(), HostError> {
+        self.mut_budget(|mut b| {
+            b.mem_bytes.reset(u64::MAX);
+            Ok(())
+        })?;
+        self.reset_tracker()
+    }
+
+    pub fn reset_tracker(&self) -> Result<(), HostError> {
+        self.0.try_borrow_mut_or_err()?.tracker.reset();
+        Ok(())
+    }
+
+    pub fn reset_limits(&self, cpu: u64, mem: u64) -> Result<(), HostError> {
+        self.mut_budget(|mut b| {
+            b.cpu_insns.reset(cpu);
+            b.mem_bytes.reset(mem);
+            Ok(())
+        })?;
+        self.reset_tracker()
+    }
+
+    /// Resets the `FuelConfig` we pass into Wasmi before running calibration.
+    /// Wasmi instruction calibration requires running the same Wasmi insn
+    /// a fixed number of times, record their actual cpu and mem consumption, then
+    /// divide those numbers by the number of iterations, which is the fuel count.
+    /// Fuel count is kept tracked on the Wasmi side, based on the `FuelConfig`
+    /// of a specific fuel category. In order to get the correct, unscaled fuel
+    /// count, we have to preset all the `FuelConfig` entries to 1.
+    pub fn reset_fuel_config(&self) -> Result<(), HostError> {
+        self.0.try_borrow_mut_or_err()?.fuel_config.reset();
+        Ok(())
+    }
+}
