9c4f48a4ab2e049e0a6308044150494f32837513
Jay Geng <jay@stellar.org>
2023-11-01 20:13:50 -0400

    try convert it into a workspace (remove the fuzz-workspace level). won't work

--- soroban-fuzz-host/debug_logging/src/lib.rs
+++ soroban-fuzz-host/debug_logging/src/lib.rs
@@ -18,19 +18,3 @@ pub fn wasm_module_with_linear_memory_logging() -> Vec<u8> {
     // fe.push(Symbol::try_from_small_str("pass").unwrap());
     fe.finish_and_export("test").finish()
 }
-
-#[test]
-fn test()
-{
-    let wasm = wasm_module_with_linear_memory_logging();
-    let host = Host::test_host_with_recording_footprint();
-    host.enable_debug().unwrap();
-    let contract_id_obj = host.register_test_contract_wasm(wasm.as_slice());
-
-    host.as_budget().reset_limits(2_000_000, 500_000).unwrap();
-    let res = host.call(
-        contract_id_obj,
-        Symbol::try_from_small_str("test").unwrap(),
-        host.test_vec_obj(&[0_u32, 10_u32, 0_u32, 10_u32]).unwrap(),
-    ).unwrap();
-}
--- soroban-fuzz-host/fuzz/fuzz_targets/fuzz_target_1.rs
+++ soroban-fuzz-host/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -1,10 +1,9 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-use fuzz_debug_logging::wasm_module_with_linear_memory_logging;
-use soroban_sdk::arbitrary::arbitrary::{self, Arbitrary};
-use soroban_sdk::arbitrary::fuzz_catch_panic;
-use soroban_env_host::{Host, Symbol, SymbolSmall, budget::AsBudget, Env};
+use debug_logging::wasm_module_with_linear_memory_logging;
+use arbitrary::Arbitrary;
+use soroban_env_host::{Host, Symbol,  budget::AsBudget, Env};
 
 #[derive(Arbitrary, Debug)]
 struct Input {
@@ -24,17 +23,14 @@ fuzz_target!(|input: Input| {
     let contract_id_obj = host.register_test_contract_wasm(wasm.as_slice());
 
     host.as_budget().reset_limits(2_000_000, 500_000).unwrap();
-    let res = host.call(
+    let _res = host.call(
         contract_id_obj,
         Symbol::try_from_small_str("test").unwrap(),
         host.test_vec_obj(&[input.msg_pos, input.msg_len, input.vals_pos, input.vals_len]).unwrap(),
     ).unwrap();
-    // assert_eq!(SymbolSmall::try_from(res).unwrap().to_string(), "pass");
-    // three debug events: fn_call, log, fn_return
-    // assert!(
-    //     !host.as_budget().shadow_mem_limit_exceeded()?
-    //         && !host.as_budget().shadow_cpu_limit_exceeded()?
-    // );
-    // let actual = format!("{}", host.as_budget());
-    // expected_budget.assert_eq(&actual);
+
+    let cpu = host.as_budget().get_cpu_insns_consumed().unwrap();
+    let mem = host.as_budget().get_mem_bytes_consumed().unwrap();
+
+    assert_eq!((cpu,mem), (1075678, 203437) );
 });
