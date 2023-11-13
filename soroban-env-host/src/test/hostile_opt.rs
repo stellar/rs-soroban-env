147d1d5923e1c8d2c7063851279633ca1a54dd06
Graydon Hoare <graydon@pobox.com>
2023-11-13 14:23:27 -0800

    Fix breakage

--- soroban-env-host/src/test/hostile_opt.rs
+++ soroban-env-host/src/test/hostile_opt.rs
@@ -1,7 +1,7 @@
 use crate::budget::AsBudget;
 use crate::builtin_contracts::base_types::Address;
 use crate::builtin_contracts::testutils::ContractTypeVec;
-use crate::{test_vec, Host, HostError, DEFAULT_HOST_DEPTH_LIMIT};
+use crate::{Host, HostError, DEFAULT_HOST_DEPTH_LIMIT};
 use soroban_builtin_sdk_macros::contracttype;
 use soroban_env_common::{Env, Symbol, TryFromVal, TryIntoVal, Val};
 use soroban_test_wasms::RECURSIVE_ACCOUNT_CONTRACT;
