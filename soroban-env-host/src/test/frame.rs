28de974fe942d524e6806007ffb483b7347d49b2
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2023-11-21 21:00:16 -0800

    warning

--- soroban-env-host/src/test/frame.rs
+++ soroban-env-host/src/test/frame.rs
@@ -7,6 +7,13 @@ use std::rc::Rc;
 
 #[test]
 fn in_frame() -> Result<(), HostError> {
+    struct NoopContractFunctionSet;
+    impl ContractFunctionSet for NoopContractFunctionSet {
+        fn call(&self, _func: &Symbol, _host: &Host, _args: &[Val]) -> Option<Val> {
+            None
+        }
+    }
+
     let host = observe_host!(Host::test_host_with_recording_footprint());
 
     // Host has no frame outside of executing a contract.
@@ -15,12 +22,6 @@ fn in_frame() -> Result<(), HostError> {
     // Host has a frame when executing a contract.
     let id = [0u8; 32];
     let address = host.add_host_object(ScAddress::Contract(Hash(id)))?;
-    struct NoopContractFunctionSet;
-    impl ContractFunctionSet for NoopContractFunctionSet {
-        fn call(&self, _func: &Symbol, _host: &Host, _args: &[Val]) -> Option<Val> {
-            None
-        }
-    }
     host.register_test_contract(address, Rc::new(NoopContractFunctionSet))?;
     let func = Symbol::try_from_small_str("")?;
     host.with_test_contract_frame(Hash(id), func, || {
