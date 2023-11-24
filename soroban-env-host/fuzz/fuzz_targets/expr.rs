0c0058c76d3a4fd37f5a9abb1775ae99bae398b4
Graydon Hoare <graydon@pobox.com>
2023-11-23 17:14:54 -0800

    Remove a bunch of redundant fuzzer code, use exported testutils.

--- soroban-env-host/fuzz/fuzz_targets/expr.rs
+++ soroban-env-host/fuzz/fuzz_targets/expr.rs
@@ -1,32 +1,43 @@
 #![no_main]
 
+use arbitrary::Arbitrary;
+use libfuzzer_sys::fuzz_target;
 use soroban_env_host::{
-    budget::Budget,
-    xdr::{Hash, HostFunction, InvokeContractArgs, ScAddress, ScErrorCode, ScErrorType, ScSymbol},
-};
-use soroban_env_host_fuzz::{
-    storage_with_test_contract, test_host_with_storage_and_budget, test_scvec, TEST_CONTRACT_ID,
-    TEST_FN_NAME,
+    xdr::{HostFunction, InvokeContractArgs, ScErrorCode, ScErrorType, ScSymbol, ScVal, VecM},
+    Host,
 };
 use soroban_synth_wasm::{Emit, Expr};
 
-use libfuzzer_sys::fuzz_target;
+// We augment the `Expr` we generate with other parameters we'd like the fuzzer to explore.
+#[derive(Arbitrary, Debug)]
+struct TestCase {
+    expr: Expr,
+    cpu_budget: u32,
+    mem_budget: u32,
+}
 
-fuzz_target!(|expr: Expr| {
-    // fuzzed code goes here
-    let wasm = expr.0.as_single_function_wasm_module("test");
+const TEST_FN_NAME: &'static str = "test";
 
-    let budget = Budget::default();
-    let storage = storage_with_test_contract(wasm, &budget);
-    let host = test_host_with_storage_and_budget(storage, budget);
-    host.enable_debug().unwrap();
+fuzz_target!(|test: TestCase| {
+    let wasm = test.expr.0.as_single_function_wasm_module(TEST_FN_NAME);
+    let (host, contract) = Host::test_host_with_wasm_and_enforcing_footprint(&wasm, &[], &[]);
+    let contract_address = host.scaddress_from_address(contract).unwrap();
+    host.with_budget(|budget| {
+        // Mask the budget down to 268m instructions / 256MiB memory so we don't
+        // literally run out of time or memory on the machine we're fuzzing on.
+        budget.reset_limits(
+            test.cpu_budget as u64 & 0x0fff_ffff,
+            test.mem_budget as u64 & 0x0ff_ffff,
+        )
+    })
+    .unwrap();
 
-    let args = test_scvec::<u32>(&[]);
+    let args: VecM<ScVal> = vec![].try_into().unwrap();
 
     let hf = HostFunction::InvokeContract(InvokeContractArgs {
-        contract_address: ScAddress::Contract(Hash(TEST_CONTRACT_ID.clone())),
+        contract_address,
         function_name: ScSymbol(TEST_FN_NAME.try_into().unwrap()),
-        args: args.try_into().unwrap(),
+        args,
     });
 
     let res = host.invoke_function(hf);
