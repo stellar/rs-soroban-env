2d0697b5f62390c33219ac63a5cef918086c1091
Jay Geng <jay@stellar.org>
2024-01-31 12:49:24 -0500

    Fix auth tracker metering charge

--- soroban-env-host/src/auth.rs
+++ soroban-env-host/src/auth.rs
@@ -849,7 +849,10 @@ impl AuthorizationManager {
         let auth_entries =
             host.visit_obj(auth_entries, |e: &HostVec| e.to_vec(host.budget_ref()))?;
         let mut trackers = self.try_borrow_invoker_contract_trackers_mut(host)?;
-        Vec::<Val>::charge_bulk_init_cpy(auth_entries.len() as u64, host)?;
+        Vec::<InvokerContractAuthorizationTracker>::charge_bulk_init_cpy(
+            auth_entries.len() as u64,
+            host,
+        )?;
         trackers.reserve(auth_entries.len());
         for e in auth_entries {
             trackers.push(
