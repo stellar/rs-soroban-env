7750136ea8b928659e213af89e96fd1fb6b50bdb
Graydon Hoare <graydon@pobox.com>
2024-07-25 15:48:05 -0700

    fix build script for rust 1.77

--- soroban-env-host/build.rs
+++ soroban-env-host/build.rs
@@ -1,7 +1,8 @@
 fn main() {
-    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo::rustc-check-cfg=cfg(opt_build)");
+    println!("cargo::rerun-if-changed=build.rs");
     let opt_level = std::env::var("OPT_LEVEL").unwrap_or_else(|_| "0".to_string());
     if opt_level != "0" {
-        println!("cargo:rustc-cfg=opt_build");
+        println!("cargo::rustc-cfg=opt_build");
     }
 }
