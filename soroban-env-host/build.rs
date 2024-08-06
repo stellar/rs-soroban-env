196cebc41ffbc6b27d3e14b1564f0bd0e9c8550b
Dmytro Kozhevin <dmytro@stellar.org>
2024-08-06 16:52:35 -0400

    !fixup fmt

--- soroban-env-host/build.rs
+++ soroban-env-host/build.rs
@@ -1,4 +1,4 @@
-fn main() {    
+fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo::rustc-check-cfg=cfg(opt_build)");
     let opt_level = std::env::var("OPT_LEVEL").unwrap_or_else(|_| "0".to_string());
