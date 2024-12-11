8ff00670505d20783c816656b4520024deaaa7aa
Dmytro Kozhevin <dmytro@stellar.org>
2024-12-11 17:08:09 -0500

    Expose structs used for invocation metering.
    These are meant to be public.

--- soroban-env-host/src/lib.rs
+++ soroban-env-host/src/lib.rs
@@ -37,6 +37,7 @@ pub use vm::Vm;
 pub mod storage;
 pub use budget::{DEFAULT_HOST_DEPTH_LIMIT, DEFAULT_XDR_RW_LIMITS};
 pub use host::{
+    invocation_metering::FeeEstimate, invocation_metering::InvocationResources,
     metered_map::MeteredOrdMap, metered_vector::MeteredVector, Host, HostError, Seed, SEED_BYTES,
 };
 pub use soroban_env_common::*;
