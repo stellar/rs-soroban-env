graydon/rs-stellar-contract-env

Remove p22/immutable mode from module cache, only support p23/mutable

soroban-env-host/src/test/invocation.rs
@@ -218,7 +218,7 @@ fn contract_failure_with_debug_on_off_affects_no_metering() -> Result<(), HostEr
 
     let invoke_cross_contract_indirect_with_err = || -> Result<(u64, u64, u64, u64), HostError> {
         // try call -- add will trap, and add_with will trap, but we will get an error
-        host.rebuild_module_cache()?;
+        host.build_module_cache_if_needed()?;
         host.as_budget().reset_default()?;
         let res = host.try_call(id0_obj, sym, args);
         HostError::result_matches_err(

stellar/rs-soroban-env

soroban-env-host/src/test/invocation.rs
@@ -218,7 +218,7 @@ fn contract_failure_with_debug_on_off_affects_no_metering() -> Result<(), HostEr
 
     let invoke_cross_contract_indirect_with_err = || -> Result<(u64, u64, u64, u64), HostError> {
         // try call -- add will trap, and add_with will trap, but we will get an error
-        host.rebuild_module_cache()?;
+        host.ensure_module_cache_contains_host_storage_contracts()?;
         host.as_budget().reset_default()?;
         let res = host.try_call(id0_obj, sym, args);
         HostError::result_matches_err(