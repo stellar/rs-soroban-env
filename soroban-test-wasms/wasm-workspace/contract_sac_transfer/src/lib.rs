add31cf4394251e3035767dc08478bb197f48e77
Siddharth Suresh <siddharth@stellar.org>
2025-04-16 10:29:58 -0700

    Add SAC balance tests (#1533)
    Add tests for balances greater than `i64::MAX`.

--- soroban-test-wasms/wasm-workspace/contract_sac_transfer/src/lib.rs
+++ soroban-test-wasms/wasm-workspace/contract_sac_transfer/src/lib.rs
@@ -15,4 +15,12 @@ impl SacTransfer {
                                    1_i128
                                   ).into_val(&env));
     }
+
+    pub fn transfer_amount(env: Env, sac_address: Address, to: Address, amount:i128) {
+        env.invoke_contract::<()>(&sac_address, &symbol_short!("transfer"),
+                                  (env.current_contract_address(),
+                                   to,
+                                   amount
+                                  ).into_val(&env));
+    }
 }
